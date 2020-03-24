3a6940c55243b96d3c9c5b6523e5787e89525acf
Rodrigo Emanuel <drigosantos@gmail.com>
2020-03-24 13:00:43 -0300

    Rota e função de consulta específica implementada.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const ongController = require('./controllers/ongController');
 const incidentController = require('./controllers/incidentController');
+const profileController = require('./controllers/profileController');
 
 const routes = express.Router();
 
 routes.get('/ongs', ongController.index);
 routes.post('/ongs', ongController.create);
+
+routes.get('/profile', profileController.index);
 
 routes.get('/incidents', incidentController.index);
 routes.post('/incidents', incidentController.create);
