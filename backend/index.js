2ef46212222974da56e7948aacd3d62e085faea3
Rodrigo Emanuel <drigosantos@gmail.com>
2020-03-24 09:46:34 -0300

    Instalado nodemon no Backend.

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', function(request, response) {
+app.use(express.json());
+
+/*
+    * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros paginação)
+    * Route Params: Parâmetros utilizados para identificar recursos
+    * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+*/ 
+
+app.post('/users', function(request, response) {
+    const body = request.body;
+
+    console.log(body);
+    
     return response.json({ 
         evento: 'Semana Omnistack 11.0',
-        aluno: 'Rodrigo Emanuel'
+        aluno: 'Rodrigo Santos'
      });
 });
 
-app.listen(3333);
+app.listen(3333, function(){
+    console.log("Server Activeted");
+});
