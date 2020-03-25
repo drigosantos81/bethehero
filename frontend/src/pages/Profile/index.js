2e91f3f320bdf20a3dff34c2a103fb0059bdb40a
Rodrigo Emanuel <drigosantos@gmail.com>
2020-03-25 19:48:01 -0300

    Criado arquivo da página de visualização dos Casos.

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower } from 'react-icons/fi';
+
+import './style.css';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function Profile() {
+    return (
+        <div className="profile-container">
+            <header>
+                <img src={logoImg} alt="Be The Hero"/>
+                <span>Bem Vindo, APAD</span>
+
+                <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
+                <button type="button">
+                    <FiPower size={18} color="#e02041" />
+                </button>
+            </header>
+
+            <h1>Casos cadastrados</h1>
+
+            <ul>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+
+                    <strong>DESCRIÇÃO:</strong>
+                    <p>Descrição teste</p>
+
+                    <strong>VALOR:</strong>
+                    <p>R$ 120,00</p>
+
+                    <button></button>
+
+                </li>
+            </ul>
+
+        </div>
+    )
+}
