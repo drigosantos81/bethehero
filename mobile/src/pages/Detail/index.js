712eda765ef45f7f76d0bb7a2a3617641e77fe5c
Rodrigo Emanuel <drigosantos@gmail.com>
2020-03-26 18:51:22 -0300

    Funções de enviar e-mail e mensagem do whatsapp feita.

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
+import * as MailComposer from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
 
@@ -9,9 +10,22 @@ import styles from './style';
 
 export default function Detail() {
     const navigation = useNavigation();
+    const message = 'Olá Celula Mãe, estou entrando em contato pois gostaria de ajudar no caso "Cachorrinho atropelado" com o valor de R$ 120,00';
 
     function navigateBack() {
         navigation.goBack();
+    }
+
+    function sendMail() {
+        MailComposer.composeAsync({
+            subject: 'Herói do caso: Cachorrinho atropelado',
+            recipients: ['drigosantos@gmail.com'],
+            body: message,
+        })
+    }
+
+    function sendWhatsapp() {
+        Linking.openURL(`whatsapp://send?phone=5571991056068&text=${message}`);
     }
 
     return (
@@ -43,11 +57,11 @@ export default function Detail() {
                 <Text style={styles.heroDescription}>Entre em contato:</Text>
 
                 <View style={styles.actions}>
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
                         <Text style={styles.actionText}>WhatsApp</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={sendMail}>
                         <Text style={styles.actionText}>E-mail</Text>
                     </TouchableOpacity>
                 </View>
