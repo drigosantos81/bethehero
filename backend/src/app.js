257a845cb7eceb895b06ee7da52cb43b016ba007
Rodrigo Emanuel <drigosantos@gmail.com>
2020-03-27 17:50:30 -0300

    Implantado ambiente de teste e primeiros testes feitos.

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,9 +11,7 @@ app.use(express.json());
 app.use(routes);
 app.use(errors());
 
-app.listen(3333, function(){
-    console.log("Server Activeted");
-});
+module.exports = app;
 
 /*
     * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros paginação)
---
+++ backend/src/server.js
@@ -0,0 +1,7 @@
+const app = require('./app');
+
+app.listen(3333, function(){
+        console.log("Server Activeted");
+    });
+
+    // app.listen(3333);
