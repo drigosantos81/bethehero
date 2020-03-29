ebb02cba90bd572c5b4be6934370e12cc6338083
Rodrigo Emanuel <drigosantos@gmail.com>
2020-03-29 13:28:33 -0300

    Fix-bug na paginação da listagem de casos. Fim do warning da tela amarela. App finalizado.

--- backend/src/controllers/incidentController.js
+++ backend/src/controllers/incidentController.js
@@ -6,11 +6,20 @@ module.exports = {
 
         const [count] = await connection('incidents').count();
 
+        // console.log(count);        
+
         const incidents = await connection('incidents')
-            .join('ongs', 'ong_id', '=', 'incidents.ong_id')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
             .offset((page - 1) * 5)
-            .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);
+            .select([
+                'incidents.*', 
+                'ongs.name', 
+                'ongs.email', 
+                'ongs.whatsapp', 
+                'ongs.city', 
+                'ongs.uf'
+            ]);
 
         response.header('X-Total-Count', count['count(*)']);
 
@@ -35,7 +44,10 @@ module.exports = {
         const { id } = request.params;
         const ong_id = request.headers.authorization;
 
-        const incident = await connection('incidents').where('id', id).select('ong_id').first();
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first();
 
         if (incident.ong_id != ong_id) {
             return response.status(401).json({ error: 'Operação não permitida' });
